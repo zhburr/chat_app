3b707d462f849d18623a809c131551dc92cf6901
zohaib ur rehman <zohaib.rehman@tkxel.io>
2022-11-05 02:26:11 +0500

    setup interceptor

---
+++ src/utils/interceptor/interceptor.ts
@@ -0,0 +1,37 @@
+import {
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+} from "axios";
+import { ApiRoutes } from "../ApiRoutes";
+
+const onRequest = (config: AxiosRequestConfig): AxiosRequestConfig => {
+  if (
+    config.url != ApiRoutes.loginRoute ||
+    config.url != ApiRoutes.registerRoute
+  ) {
+    //add token to header in future
+  }
+  return config;
+};
+
+const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+  return Promise.reject(error);
+};
+
+const onResponse = (response: AxiosResponse): AxiosResponse => {
+  return response;
+};
+
+const onResponseError = (error: AxiosError): Promise<AxiosError> => {
+  return Promise.reject(error);
+};
+
+export function setupInterceptorsTo(
+  axiosInstance: AxiosInstance
+): AxiosInstance {
+  axiosInstance.interceptors.request.use(onRequest, onRequestError);
+  axiosInstance.interceptors.response.use(onResponse, onResponseError);
+  return axiosInstance;
+}
